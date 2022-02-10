4e4d83d605090aa7fb1508663a3135fe4908212b
Ekaterina Yakimuk <92802927+Yakimukk@users.noreply.github.com>
2022-02-10 12:48:28 +0200

    fix: bug fixes

--- js/script.js
+++ js/script.js
@@ -1,67 +1,17 @@
 
-var slider = document.querySelector('.FAQ-container');
-
+const slider = document.querySelector('.swiper-container');
 if (slider) {
-  new Swiper('.FAQ-container', {
-
-    slidesPerView: 'auto',
-
- 
-    centeredSlidesBounds: true,
-
-  
-
-    spaceBetween: 24,
-
- 
-    speed: 500,
-
-
-    loop: true,
-
-
-    navigation: {
-      nextEl: '.FAQ-card-arrow-prev',
-      prevEl: '.FAQ-card-arrow-next',
-    },
-
-
-    breakpoints: {
-      // 540: {
-      //   slidesPerView: 'auto',
-      // },
-      768: {
-        slidesPerView: 'auto',
-        spaceBetween: 0,
+    new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      centeredSlidesBounds: true,
+      spaceBetween: 24,
+      speed: 500,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
       },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 10,
-      },
-    },
-
-
-    watchOverflow: true,
-
-
-  });
+      watchOverflow: true,
+    });
 }
-
-/* let width = 282; // ширина картинки
-
-
-let list = document.querySelector('.FAQ-cards')
-let listElems = document.querySelectorAll('.FAQ-card')
-
-let position = 0; // положение ленты прокрутки
-
-
-document.querySelector('.FAQ-card-arrow').onclick = function() {
-    position -= width 
-    position = Math.min(position, 0)
-    list.style.marginLeft = position + 'px'
-} */
-
-
 
 
